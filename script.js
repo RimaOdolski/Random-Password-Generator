a5ff2c3282ed43d2bf927dbb406c5da66ea279f5
Rima O <rymaothmani@gmail.com>
2020-01-05 20:00:16 -0600

    create PW generator initial prompts

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function  getRandomLower() {
-  return String.fromCharCode(math.floor(math.radom() * 26) + 97);
+var pLength = prompt("Choose a length of your password between 8 and 128 characters");
+
+if (pLength < 8 || pLength > 128) {
+alert("Length must between 8-128 characters");
 }
-console.log("test");
+
+else 
+
+var specialChar = confirm(" Would you like to have any special characters in your Password?");
+var numChar = confirm("Would you like to have a numeric character in your password?");
+var lowerCase = confirm("would you like to have lower case characters");
+var upperCase = confirm("would you like to have upper case characters");
+
+
+
+//Generate PW
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log (getRandomNumber());
