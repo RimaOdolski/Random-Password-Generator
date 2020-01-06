61818682bb52a697aeb8ca11aec852803638d68c
Rima O <rymaothmani@gmail.com>
2020-01-06 01:29:44 -0600

    declare variables for numbers,lowecase,uppercase and special characters and create user prompts

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-var pLength = prompt("Choose a length of your password between 8 and 128 characters");
 
-if (pLength < 8 || pLength > 128) {
-alert("Length must between 8-128 characters");
+function generatePassword() {
+
+  //////////////////////////////////////////////////
+
+  var allNumbers = "0123456789";
+  var allSpecial = "!@#$%^&*()_+[]{}";
+  var allUpper = "ABCDEFHGIJKLMNOPQRSTUVWXYZ";
+  var allLower = "abcdefghijklmnopqrstuvwxyz";
+  var passwordParameters = "";
+  var newPassword = "";
+
+  ///////////////////////////////////////////////////
+
+  var passwordLength = prompt("Choose the length of your password between 8 and 128 characters.");
+  if ((parseInt(passwordLength)) >= 8 || (parseInt(passwordLength)) <= 128) {
+    var userNumbers = prompt("Do you want numericals in your password?");
+    if (userNumbers == true) {
+      passwordParameters += allNumbers;
+    }
+    var userSpecial = prompt("Do you want special characters in your password?");
+    if (userSpecial == true) {
+      passwordParameters += allSpecial;
+    }
+    var userUpper = prompt("Do you want to have upper case characters in your password?");
+    if (userUpper == true) {
+      passwordParameters += userUpper;
+    }
+    var userLower = prompt("Do you want to have lower case characters in your password?");
+    if (userLower == true) {
+      passwordParameters += userLower;
+    }
+
+  else {
+    alert("The length of the password must between 8 - 128 characters.");
+  }
 }
-
-else 
-
-var specialChar = confirm(" Would you like to have any special characters in your Password?");
-var numChar = confirm("Would you like to have a numeric character in your password?");
-var lowerCase = confirm("would you like to have lower case characters");
-var upperCase = confirm("would you like to have upper case characters");
-
-
-
-//Generate Random Numbers
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-console.log (getRandomNumber());
-
-//Generate Random Lower case 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
-}
-console.log (getRandomLower());
-
-//Generate Random Upper case
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() *26) +65);
-}
-console.log (getRandomUpper());
-
-//Generate Random Special Character 
-function getRandomSpecial() {
-  const specialChar = '!@#$%^&*(){}[]=<>/,.';
-  return specialChar[Math.floor(Math.random() * specialChar.length)];
-}
-console.log (getRandomSpecial());
-
